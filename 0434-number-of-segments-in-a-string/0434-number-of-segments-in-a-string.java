84d8b0be4cf090c0826ae2fcb60c26cb56dbf930
Harshal Ahire <157964378+harshal14ahire@users.noreply.github.com>
2024-12-30 20:12:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countSegments(String s) {
+        if (s.equals(""))
+        return 0;
+        String str[]= s.split(" ");
+        return str.length;
+        
+
+    }
+}
