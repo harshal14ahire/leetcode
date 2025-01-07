00ee71b4492c32663f7849b02e668f93d7555fe7
Harshal Ahire <157964378+harshal14ahire@users.noreply.github.com>
2025-01-07 23:37:07 +0530

    Time: 1 ms (17.51%), Space: 46.1 MB (38.85%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,20 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] out = new int[2];
-        out[0] = -1;
-        out[1] = -1;
-        
-        // Binary search for the first occurrence of the target
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int result [] = new int[2];
+        result [0] = -1;
+        result [1] = -1;
+        for(int i=0;i<nums.length;i++){ // find target on front side
+            if(nums[i]==target){
+                result[0] = i;
+                break;
             }
         }
-        if (left < nums.length && nums[left] == target) {
-            out[0] = left;
-        } else {
-            return out;  // If the first occurrence is not found, no need to proceed
-        }
-
-        // Reset the variables for the second binary search
-        left = 0;
-        right = nums.length - 1;
-        
-        // Binary search for the last occurrence of the target
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        for(int i=nums.length-1;i>=0;i--){ // find target on back side
+            if(nums[i]==target){
+                result[1] = i;
+                break;
             }
         }
-        if (right >= 0 && nums[right] == target) {
-            out[1] = right;
-        }
-
-        return out;
+        return result;
     }
 }
