56af5f111c88f799870999f108a69e7d1b802d6c
Harshal Ahire <157964378+harshal14ahire@users.noreply.github.com>
2025-01-23 16:34:56 +0530

    Time: 741 ms (23.66%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,4 +1,3 @@
-# Write your MySQL query statement below
-SELECT patient_id ,patient_name,conditions
+SELECT patient_id, patient_name, conditions
 FROM Patients
-WHERE  conditions REGEXP '[[:<:]]DIAB1[[:>:]]';
+WHERE conditions REGEXP '(^| )DIAB1';
