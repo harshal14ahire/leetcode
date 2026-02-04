3bc8ec720d19c20940a67caf02db9d1ca1aa0ffa
Harshal Ahire <157964378+harshal14ahire@users.noreply.github.com>
2026-02-04 22:21:33 +0530

    Time: 32 ms (5.14%), Space: 53.6 MB (5.28%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,3 +1,4 @@
+import java.util.*;
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Arrays.sort(nums);
@@ -6,14 +7,10 @@ class Solution {
          for( int i =0;i<nums.length;i++){
            map.merge(nums[i], 1, Integer::sum);
          }
-         Map<Integer, Integer> sortedMap = map.entrySet() .stream()
-            .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
-            .collect(Collectors.toMap(
-                Map.Entry::getKey, 
-                Map.Entry::getValue, 
-                (oldValue, newValue) -> oldValue, 
-                LinkedHashMap::new 
-            ));
-         return sortedMap.keySet().stream().limit(k).mapToInt(Integer::intValue) .toArray();
+     return map.entrySet().stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .limit(k)                                                    
+                .mapToInt(Map.Entry::getKey)                                 
+                .toArray();
     }
 }
