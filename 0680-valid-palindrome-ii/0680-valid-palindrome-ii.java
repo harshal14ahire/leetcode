1c8465f78d4c2395734a5c9f22a7411f6f489a47
Harshal Ahire <157964378+harshal14ahire@users.noreply.github.com>
2026-02-08 21:18:20 +0530

    Time: 10 ms (5.25%), Space: 48.1 MB (10.8%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -14,13 +14,8 @@ class Solution {
     }
     
     public boolean isPalindrome(String s) {
-    // Create the builder with the string content
     StringBuilder sb = new StringBuilder(s);
-    
-    // Reverse the content in-place
     String reversed = sb.reverse().toString();
-    
-    // Check if the original equals the reversed
     return s.equals(reversed);
 }
 }
